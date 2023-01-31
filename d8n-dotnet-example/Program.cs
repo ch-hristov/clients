8e70ab059ec8c7fb7fc269f8615eb34fad7a32f8
Christo S. Christov <ch-hristov@users.noreply.github.com>
2023-01-31 06:20:15 -0500

    Remove cached info

---
+++ d8n-dotnet-example/Program.cs
@@ -0,0 +1,7 @@
+﻿using D8nObjectDetection;
+
+DocumentClient client = new DocumentClient("");
+var result = await client.RunAnalysis("./test.jpg");
+
+await Task.Delay(3000);
+var objects = await client.GetCompleted(result);
